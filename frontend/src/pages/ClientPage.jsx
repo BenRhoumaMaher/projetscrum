aefee62d386c72f4eab0fcf1a65a9e87a3aa06ef
maher ben rhouma <maherbenrhoumaa@gmail.com>
2024-12-05 09:01:14 +0100

    realisation de user story 12

--- frontend/src/pages/ClientPage.jsx
+++ frontend/src/pages/ClientPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import ClientDashboard from '../components/ClientDashboard'; // Assurez-vous du bon chemin d'import
 
 const ClientPage = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('userToken');
+        alert('Vous êtes déconnecté.');
+        navigate('/');
+    };
+
     return (
         <div className="container mt-5">
-            <p>bonjour</p>
+            <h2 className="mb-4 text-center">Espace Client</h2>
+            <ClientDashboard onLogout={handleLogout}/>
         </div>
     );
 };
