7dfbf39551481479264e671008f32edec9ef3bcd
maher ben rhouma <maherbenrhoumaa@gmail.com>
2024-12-10 10:00:50 +0100

    realisation de user story 8 frontend

--- frontend/src/components/ProductListComponent.jsx
+++ frontend/src/components/ProductListComponent.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -20,6 +21,25 @@ const ProductList = () => {
 
     const handleEdit = (product) => {
         navigate('/edit-product', { state: { product } });
+    };
+
+    const handleDelete = async (id) => {
+        if (window.confirm('Êtes-vous sûr de vouloir supprimer ce produit?')) {
+            try {
+                const response = await fetch(`http://localhost:5000/api/products/${id}`, {
+                    method: 'DELETE',
+                });
+
+                if (response.ok) {
+                    setProducts(products.filter((product) => product._id !== id));
+                } else {
+                    setErrorMessage('Erreur lors de la suppression du produit.');
+                }
+            } catch (error) {
+                console.error('Erreur lors de la suppression:', error);
+                setErrorMessage('Une erreur est produite.');
+            }
+        }
     };
 
     return (
@@ -46,6 +66,12 @@ const ProductList = () => {
                                     onClick={() => handleEdit(product)}
                                 >
                                     Modifier
+                                </button>
+                                <button
+                                    className="btn btn-danger btn-sm"
+                                    onClick={() => handleDelete(product._id)}
+                                >
+                                    Supprimer
                                 </button>
                             </td>
                         </tr>
